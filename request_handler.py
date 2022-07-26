0f8c5f522ba2aca650526b7f87f00347ad3d2488
Griffin Moyer <moyerga@outlook.com>
2022-07-26 14:08:34 -0500

    fixed endpoint on create_post so that it matches fetch call on front end

--- request_handler.py
+++ request_handler.py
@@ -67,7 +67,7 @@ class HandleRequests(BaseHTTPRequestHandler):
             response = login_user(post_body)
         if resource == 'register':
             response = create_user(post_body)
-        if resource == 'post':
+        if resource == 'posts':
             response = create_post(post_body)
 
         self.wfile.write(response.encode())
