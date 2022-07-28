62587360e26f0b0e238db63f2ced073c0a1af3d7
Vicki Taylor <vtaylor85@live.com>
2022-07-28 14:38:50 -0500

    commit to pull main

--- views/__init__.py
+++ views/__init__.py
@@ -1,4 +1,4 @@
 from .post_requests import create_post, get_posts_by_user_id, get_all_posts, get_single_post
 from .category_requests import get_all_categories, create_category, delete_category, edit_category
 from .comment_requests import get_all_comments, create_comment
-from .tag_requests import get_all_tags, create_tag, delete_tag
+from .tag_requests import get_all_tags, create_tag, delete_tag, edit_tag
